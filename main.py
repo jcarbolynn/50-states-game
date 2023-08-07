5ceeb842511e09dda102cc45dc4a42521f6a6c1c
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:49:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,42 @@
 import turtle
 import pandas
 
-ALIGNMENT = "center"
-FONT = ("Courier", 10, "normal")
-
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-print_state = turtle.Turtle()
-print_state.penup()
-print_state.hideturtle()
+data = pandas.read_csv("50_states.csv")
+# data series, first column in data frame
+# data.state
+all_states = data.state.to_list()
+guessed_states = []
 
-states = pandas.read_csv("50_states.csv")
+while len(guessed_states) < 50:
+  answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="Name another state").title()
+  
+  # if answer_state is one of the states in all states of 50_states.csv
+  if answer_state == "Exit":
+    missing_states = []
+    for state in all_states:
+      if state not in guessed_states:
+        missing_states.append(state)
+    new_data = pandas.DataFrame(missing_states)
+    new_data.to_csv("states_to_learn.csv")
+    break
+  if answer_state in all_states:
+    guessed_states.append(answer_state)
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    # t.write(answer_state)
+    # item() takes first element of series
+    # t.write(type(state_data.state))
+    t.write(state_data.state.item())
+    # if they got it right create a turtle to write the name of the state at the states x and y
+  
+turtle.exitonclick()
 
-def state_xcoor(state):
-  xcoor = int(states[states.state == state].x)
-  return xcoor
-
-def state_ycoor(state):
-  ycoor = int(states[states.state == state].y)
-  return ycoor
-
-# # to print whole row by state name
-# for state in states.state:
-#   print(states[states.state == state])
-
-still_guessing = True
-states_guessed = 0
-while still_guessing:
-  answer_state = (screen.textinput(title = f"Guess the state {states_guessed}/50", prompt="What's another state's name?")).title()
-
-  for state in states.state:
-    if state == answer_state:
-      print_state.goto(state_xcoor(state), state_ycoor(state))
-      print_state.write(f"{answer_state}", align=ALIGNMENT, font=FONT)
-      states_guessed += 1
-
-  if states_guessed == 50:
-    still_guessing = False
-
-# if states[states.state == answer_state]:
-#   print(answer_state)
-
-turtle.mainloop()
-############################ANGELA DID########
-# # to find x and y coordinates on map image so we can use them to print names of states in correct locations
-# def get_mouse_click_coor(x,y):
-#   print(x,y)
-# turtle.onscreenclick(get_mouse_click_coor)
-
-# # alternative to exitonclick() to keep the screen open
-# # turtle.mainloop()
-##############################################
